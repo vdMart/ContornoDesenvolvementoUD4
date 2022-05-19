191864f634cad420aed952ce2657e8b58059d748
Martin <Martin@martin.gal>
2022-05-19 16:26:24 -0400

    tarea

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,125 @@
+package cuentas;
+
+/**
+ * Tarea ED04
+ * @author Martín Vázquez Davila
+ */
+public class CCuenta {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    /**
+     * Metodo constructor vacio
+     */
+    public CCuenta()
+    {
+    }
+
+    /**
+     * Metodo constructor con los siguientes parametros
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    /**
+     * @return el saldo
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     * metodo que ingresa una cantidad modificando el saldo
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * metodo que retira una cantidad modificando el saldo
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
---
+++ src/cuentas/Main.java
@@ -0,0 +1,43 @@
+package cuentas;
+
+/**
+ * Tarea ED04
+ * @author Martín Vázquez Davila
+ */
+public class Main {
+
+    /**
+     * Metodo principal
+     * @param args
+     */
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        
+        operativa_cuenta(cuenta1, saldoActual, 0.0f);
+    }
+
+    /**
+     * Metodo que trabaja con las sentencias que operan con el objeto cuenta1.
+     * @param cuenta1
+     * @param saldoActual
+     * @param cantidad
+     */
+    public static void operativa_cuenta(CCuenta cuenta1,double saldoActual, float cantidad) {
+        System.out.println("El saldo actual es: "+ saldoActual );
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
